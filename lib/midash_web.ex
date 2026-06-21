3ddbf07479dc08f71d205893fee1db422fa7cb1b
Always blue <bluesky.1289@gmail.com>
2026-06-21 08:29:32 +0700

    Cleanup unused components

--- lib/midash_web.ex
+++ lib/midash_web.ex
@@ -50,23 +50,6 @@ defmodule MidashWeb do
     end
   end
 
-  def live_view do
-    quote do
-      use Phoenix.LiveView,
-        layout: {MidashWeb.Layouts, :app}
-
-      unquote(html_helpers())
-    end
-  end
-
-  def live_component do
-    quote do
-      use Phoenix.LiveComponent
-
-      unquote(html_helpers())
-    end
-  end
-
   def html do
     quote do
       use Phoenix.Component
@@ -89,11 +72,6 @@ defmodule MidashWeb do
       import Phoenix.HTML
       # Core UI components
       import MidashWeb.CoreComponents
-      import MidashWeb.DashboardComponents
-      import Lucideicons, except: [link: 1, import: 1, quote: 1]
-
-      # Shortcut for generating JS commands
-      alias Phoenix.LiveView.JS
 
       # Routes generation with the ~p sigil
       unquote(verified_routes())
